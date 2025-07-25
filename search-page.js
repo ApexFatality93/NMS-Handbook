18006ab6219aa57c6c5b773a929886aaa11612df
ApexFatality93 <90066549+ApexFatality93@users.noreply.github.com>
2025-07-25 14:01:31 -0500

    fixed header menu dropdown in search.html

--- search-page.js
+++ search-page.js
@@ -122,3 +122,18 @@ function createSearchCard(item) {
 
     resultsGrid.appendChild(card);
 }
+
+// Header menu dropdown function
+function showDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
